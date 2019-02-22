799d4334f562ad130dab7658ce1ca2e96b893582
cacels <chrisillals@gmail.com>
2019-02-22 14:28:05 +0700

    Initial commit

---
+++ app/src/main/java/android/cacels/com/keyboarddialphone/MainActivity.java
@@ -0,0 +1,49 @@
+package android.cacels.com.keyboarddialphone;
+
+import android.content.Intent;
+import android.net.Uri;
+import android.nfc.Tag;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.KeyEvent;
+import android.view.inputmethod.EditorInfo;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = null;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        EditText editText=findViewById(R.id.editText_main);
+        if (editText != null);
+        editText.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+                boolean handled = false;
+                if (actionId == EditorInfo.IME_ACTION_SEND) {
+                    dialNumber();
+                    handled = true;
+                }
+                return handled;
+            }
+        });
+    }
+    private void dialNumber(){
+        EditText editText = findViewById(R.id.editText_main);
+        String phoneNum = null;
+        if (editText != null) phoneNum = "tel:" + editText.getText().toString();
+        Log.d(TAG, "dialNumber" + phoneNum);
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse(phoneNum));
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d("ImplicitIntents","Can't handle this!");
+        }
+    }
+}
